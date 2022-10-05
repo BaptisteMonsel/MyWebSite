2543ecc1c481fbce0283d656e23f1547cb9bb7da
Baptiste Monsel <baptiste.monsel2@gmail.com>
2022-10-05 14:32:49 +0200

    cvdemoi

---
+++ cvmoi.js
@@ -0,0 +1,34 @@
+
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+        document.getElementById("mySidenav").style.position = "absolute";
+            document.getElementById("mySidenav").style.top = "0";
+                document.getElementById("mySidenav").style.bottom = "0";
+            document.getElementById("mySidenav").style.marginLeft = "-250px";
+    document.getElementById("main").style.marginLeft = "250px";
+    document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+    document.getElementById("mySidenav").style.visibility = "visible";
+    document.getElementById("main").setAttribute('aria-hidden','true');
+    var mainLinks = document.getElementById("main").getElementsByTagName("a");
+    for (var i = 0; i < mainLinks.length; i++) {
+      mainLinks[i].setAttribute('tabindex', '-1');
+    }
+    setTimeout(function(){ document.getElementById("closenav").focus(); }, 500);
+}
+
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("main").style.marginLeft= "0";
+    document.body.style.backgroundColor = "white";
+    document.getElementById("mySidenav").style.visibility = "hidden";
+    document.getElementById("main").removeAttribute('aria-hidden');
+    var mainLinks = document.getElementById("main").getElementsByTagName("a");
+    for (var i = 0; i < mainLinks.length; i++) {
+      mainLinks[i].removeAttribute('tabindex');
+    }
+    var mainButtons = document.getElementById("main").getElementsByTagName("button");
+    for (var i = 0; i < mainButtons.length; i++) {
+      mainButtons[i].removeAttribute('disabled');
+    }
+    document.getElementById("opennav").focus();
+}
