4d82bde14981db7e89289f167f9a9fc21fddd7c1
Baptiste Monsel <baptiste.monsel2@gmail.com>
2022-10-18 16:39:25 +0200

    rajout css

--- cvmoi.js
+++ cvmoi.js
@@ -1,34 +1,30 @@
+$( document ).ready(function() {
+  function initMenu() {
+    $( ".cross" ).hide();
+    $( ".hamburger" ).hide();
+    $( ".menu" ).show();
+    if( $('body').width()<768 ){
+      $( ".hamburger" ).show();
+      $( ".cross" ).hide();
+      $( ".menu" ).hide();
+    }
+  }
+  $( ".hamburger" ).click(function() {
+    $( ".menu" ).slideToggle( "slow", function() {
+      $( ".hamburger" ).hide();
+      $( ".cross" ).show();
+    });
+  });
+  $( ".cross" ).click(function() {
+    $( ".menu" ).slideToggle( "slow", function() {
+      $( ".cross" ).hide();
+      $( ".hamburger" ).show();
+    });
+  });
 
-function openNav() {
-    document.getElementById("mySidenav").style.width = "250px";
-        document.getElementById("mySidenav").style.position = "absolute";
-            document.getElementById("mySidenav").style.top = "0";
-                document.getElementById("mySidenav").style.bottom = "0";
-            document.getElementById("mySidenav").style.marginLeft = "-250px";
-    document.getElementById("main").style.marginLeft = "250px";
-    document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
-    document.getElementById("mySidenav").style.visibility = "visible";
-    document.getElementById("main").setAttribute('aria-hidden','true');
-    var mainLinks = document.getElementById("main").getElementsByTagName("a");
-    for (var i = 0; i < mainLinks.length; i++) {
-      mainLinks[i].setAttribute('tabindex', '-1');
-    }
-    setTimeout(function(){ document.getElementById("closenav").focus(); }, 500);
-}
+  initMenu();
+  $( window ).resize(function() {
+    initMenu();
+  });
 
-function closeNav() {
-    document.getElementById("mySidenav").style.width = "0";
-    document.getElementById("main").style.marginLeft= "0";
-    document.body.style.backgroundColor = "white";
-    document.getElementById("mySidenav").style.visibility = "hidden";
-    document.getElementById("main").removeAttribute('aria-hidden');
-    var mainLinks = document.getElementById("main").getElementsByTagName("a");
-    for (var i = 0; i < mainLinks.length; i++) {
-      mainLinks[i].removeAttribute('tabindex');
-    }
-    var mainButtons = document.getElementById("main").getElementsByTagName("button");
-    for (var i = 0; i < mainButtons.length; i++) {
-      mainButtons[i].removeAttribute('disabled');
-    }
-    document.getElementById("opennav").focus();
-}
+});
